8672da0535e303d71a8b37a6493ed235399297ee
Isaiah Becker-Mayer <isaiah@goteleport.com>
2023-12-12 18:09:39 -0800

    Create panic example

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,18 @@ use std::time::{Duration, Instant};
 fn main() {
     let mini_tokio = MiniTokio::new();
 
-    mini_tokio.spawn(async {
-        let when = Instant::now() + Duration::from_millis(10);
+    let i = 0;
+    // for i in 0..50 {
+    mini_tokio.spawn(async move {
+        println!("delay {} started!", i);
+        let when = Instant::now() + Duration::from_millis(10 * i);
         let future = Delay::new(when);
 
         let out = future.await;
         assert_eq!(out, "done");
-        println!("delay completed; out = {:?}", out);
+        println!("delay {} completed; out = {:?}", i, out);
     });
+    // }
 
     println!("Running mini_tokio...");
     mini_tokio.run();
